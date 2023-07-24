ffc58f721d362a68b8cf00638b30ad6a87af8db0
Danny Cho <dannycho.dev@gmail.com>
2023-07-23 17:21:53 -0700

    CSS done for mobile view.

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,12 @@ export default function Home() {
 				</div>
 				<div className={styles.birthdayDiv__dayDiv}>
 					<label htmlFor="" className={styles.birthdayDiv__dayDiv__label}>DAY</label>
-					<input type="text" className={styles.birthdayDiv__dayDiv__errorMessage}/>
+					<input type="text" className={styles.birthdayDiv__dayDiv__input}/>
 					<p className={styles.birthdayDiv__dayDiv__errorMessage}>Must be a valid day</p>
 				</div>
 				<div className={styles.birthdayDiv__yearDiv}>
 					<label htmlFor="" className={styles.birthdayDiv__yearDiv__label}>YEAR</label>
-					<input type="text" className={styles.birthdayDiv__yearDiv__errorMessage}/>
+					<input type="text" className={styles.birthdayDiv__yearDiv__input}/>
 					<p className={styles.birthdayDiv__yearDiv__errorMessage}>Must be a valid year</p>
 				</div>
 			</div>
@@ -27,13 +27,13 @@ export default function Home() {
 			</div>
 			<div className={styles.resultDiv}>
 				<p className={styles.resultDiv__years}>
-					<span className={styles.resultDiv__years__number}>--</span> years
+					<span className={styles.resultDiv__years__number}>38</span> years
 				</p>
 				<p className={styles.resultDiv__months}>
-					<span className={styles.resultDiv__months__number}>--</span> months
+					<span className={styles.resultDiv__months__number}>3</span> months
 				</p>
 				<p className={styles.resultDiv__days}>
-					<span className={styles.resultDiv__days__number}>--</span> days
+					<span className={styles.resultDiv__days__number}>26</span> days
 				</p>
 			</div>
 		</div>
